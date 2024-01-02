c1cfe77b137c78bf74d9f1b804afb717a63bf8f1
Your Name <abdulmuneemadnan77@gmail.com>
2024-01-02 07:27:07 +0600

    Update

---
+++ StoreManagementSystemAPI/StoreManagementSystemAPI/Controllers/ManageOrderController.cs
@@ -0,0 +1,123 @@
+﻿using BusinessLogicLayer.DTOs;
+using BusinessLogicLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StoreManagementSystemAPI.Controllers
+{
+    public class ManageOrderController : ApiController
+    {
+        [HttpGet]
+        [Route("api/orders/details/with/product/{id}")]
+        public HttpResponseMessage OrderDetailsWithProduct(int id)
+        {
+            try
+            {
+                var data = ManageOrderService.GetwithProduct(id);
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
+            }
+        }
+        [HttpGet]
+        [Route("api/orders/details/{id}")]
+        public HttpResponseMessage OrderDetails(int id)
+        {
+            try
+            {
+                var data = ManageOrderService.UserOrderDetails(id);
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
+            }
+        }
+        [HttpGet]
+        [Route("api/orders/products/details/{id}")]
+        public HttpResponseMessage OrderProductDetails(int id)
+        {
+            try
+            {
+                var data = ManageOrderService.UserOrderProductDetails(id);
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        [Route("api/order/add")]
+        public HttpResponseMessage Add(OrderDTO obj)
+        {
+            try
+            {
+                var res = OrderService.Create(obj);
+                if (res)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Inserted", Data = obj });
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Msg = "Not Inserted", Data = obj });
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Msg = ex.Message, Data = obj });
+            }
+        }
+
+        [HttpPost]
+        [Route("api/order/update")]
+        public HttpResponseMessage Update(OrderDTO obj)
+        {
+            try
+            {
+                var res = OrderService.Update(obj);
+                if (res)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Message = "Updated", Data = obj });
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = "Not Updated", Data = obj });
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message, Data = obj });
+            }
+        }
+
+        [HttpPost]
+        [Route("api/order/delete/{id}")]
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+
+                return Request.CreateResponse(HttpStatusCode.OK, OrderService.Delete(id));
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+
+            }
+        }
+    }
+}
--- StoreManagementSystemAPI/StoreManagementSystemAPI/Controllers/StaffController.cs
+++ StoreManagementSystemAPI/StoreManagementSystemAPI/Controllers/StaffController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLogicLayer.DTOs;
+using BusinessLogicLayer.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +11,32 @@ namespace StoreManagementSystemAPI.Controllers
 {
     public class StaffController : ApiController
     {
+        [HttpGet]
+        [Route("API/ViewProduct/{Id}")]
+        public HttpResponseMessage ViewProduct(int id)
+
+        {
+
+            try
+
+            {
+
+                var data = SearchService.ViewProduct(id);
+
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
+
+            }
+
+        }
+
     }
+
 }
